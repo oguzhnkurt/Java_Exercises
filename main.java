6eb5b02d4d0096886c77abc38c45c2c2f8a34ee1
Oğuzhan Kurt <73943292+oguzhnkurt@users.noreply.github.com>
2023-05-04 16:04:00 +0300

    Ortalama_Hesaplama
    Patika.Dev Pratik Ödevi

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int mat,fiz,kimya;
+
+
+
+        System.out.println("mat not giriniz : ");
+        mat = input.nextInt();
+        System.out.println("fiz not giriniz : ");
+        fiz = input.nextInt();
+        System.out.println("kimya not giriniz : ");
+        kimya = input.nextInt();
+
+
+        int ort = (mat + fiz + kimya ) /3;
+        System.out.println("ort : " +ort);
+        System.out.println(ort > 60 ? "sınıfı geçti" : "sınıfta kaldı");
+    }
+}
